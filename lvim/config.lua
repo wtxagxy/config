92dce19e4de215a0cc56fe197b0bcc0e0ec30c66
王天星 <wangtianxing@playdayy.com>
2023-01-18 17:20:04 +0800

    修改Lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,8 +11,8 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
--- lvim.colorscheme = "lunar" -- "lunar", gruvbox
-lvim.colorscheme = "gruvbox" -- "lunar", gruvbox
+lvim.colorscheme = "lunar" -- "lunar", gruvbox
+-- lvim.colorscheme = "gruvbox" -- "lunar", gruvbox
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
